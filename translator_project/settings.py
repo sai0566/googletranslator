2175a3e940d65ab86e1c6673487ffcdcfa227fcd
sai0566 <sreeram0566@gmail.com>
2026-02-11 15:56:43 +0530

    second change

--- translator_project/settings.py
+++ translator_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*16jam4eau#+7nc=&hw&dlszxb3vy8n0d5pcx5$$gm!lw7zx-%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
